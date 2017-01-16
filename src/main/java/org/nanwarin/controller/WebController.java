fc379c8baf3a2479b405ff11e1b9b3c394f0b51b
Nanwarin Chantarutai <nan2iz.ch@gmail.com>
2017-01-16 00:36:53 -0800

    initial commit

---
+++ src/main/java/org/nanwarin/controller/WebController.java
@@ -0,0 +1,13 @@
+package org.nanwarin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	/*@RequestMapping("/")
+	String home(){
+		return "Hello ! This is home";
+	}*/
+}
